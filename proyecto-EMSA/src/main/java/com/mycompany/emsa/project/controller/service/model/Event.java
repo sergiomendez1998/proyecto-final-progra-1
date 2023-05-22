47a10ca99148b245f57c8b513ab7d6809b5903d6
sergio mendez <sergiomendezgzalez@gmail.com>
2023-05-22 16:44:06 -0600

    adding new atribute for  event location

--- proyecto-EMSA/src/main/java/com/mycompany/emsa/project/controller/service/model/Event.java
+++ proyecto-EMSA/src/main/java/com/mycompany/emsa/project/controller/service/model/Event.java
@@ -14,15 +14,17 @@ public class Event {
     private int idEvent;
     private String name;
     private String description;
+    private String eventLocation;
     private Date eventDate;
     private Time startTime;
     private Time endTime;
     private String status;
 
-    public Event(String name, String description, Date eventDate, Time startTime) {
+    public Event(String name, String description, String eventLocation, Date eventDate, Time startTime) {
         this.name = name;
         this.description = description;
         this.eventDate = eventDate;
         this.startTime = startTime;
+        this.eventLocation = eventLocation;
     }
 }
--- proyecto-EMSA/src/main/java/com/mycompany/emsa/project/controller/service/model/User.java
+++ proyecto-EMSA/src/main/java/com/mycompany/emsa/project/controller/service/model/User.java
@@ -1,6 +1,6 @@
 package com.mycompany.emsa.project.controller.service.model;
 
-import lombok.AllArgsConstructor;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
